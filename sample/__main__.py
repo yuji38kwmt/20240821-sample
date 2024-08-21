bd31db11a466207777f3803ac59de4c3e3347ced
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2024-08-21 14:54:08 +0900

    update

--- sample/__main__.py
+++ sample/__main__.py
@@ -1,15 +1,11 @@
-import logging.config
 import logging
-
 from sample.utils import load_logging_config
 
 logger = logging.getLogger(__name__)
 
-
 def main():
     load_logging_config()
     logger.info("hello world")
-
 
 if __name__ == "__main__":
     main()
--- sample/utils.py
+++ sample/utils.py
@@ -5,6 +5,6 @@ import pkgutil
 
 
 def load_logging_config() -> None:
-    data = pkgutil.get_data("annofabcli", "data/logging.yaml")
+    data = pkgutil.get_data("sample", "data/logging.yaml")
     logging_config = yaml.safe_load(data.decode("utf-8"))
     logging.config.dictConfig(logging_config)
