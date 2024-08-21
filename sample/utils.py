84366f425bb425a3c4b68ae364398636c659352a
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2024-08-21 14:44:30 +0900

    add file

--- sample/utils.py
+++ sample/utils.py
@@ -5,6 +5,5 @@ from pathlib import Path
 
 def load_logging_config() -> None:
     logging_config_file = Path(__file__).parent / "data/logging.yaml"
-    print(f"{logging_config_file=}")
     logging_config = yaml.safe_load(logging_config_file.open())
     logging.config.dictConfig(logging_config)
