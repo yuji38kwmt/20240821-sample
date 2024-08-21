bb5274553498d47bb97739e2f6aa62807768e811
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2024-08-21 14:50:25 +0900

    update

--- sample/utils.py
+++ sample/utils.py
@@ -1,9 +1,10 @@
 import logging
 import yaml
 from pathlib import Path
+import pkgutil
 
 
 def load_logging_config() -> None:
-    logging_config_file = Path(__file__).parent / "data/logging.yaml"
-    logging_config = yaml.safe_load(logging_config_file.open())
+    data = pkgutil.get_data("annofabcli", "data/logging.yaml")
+    logging_config = yaml.safe_load(data.decode("utf-8"))
     logging.config.dictConfig(logging_config)
