d712a04462f8d59024ebc24039ba9b69b6c4c3ed
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2024-08-21 12:31:51 +0900

    update

--- sample/__main__.py
+++ sample/__main__.py
@@ -4,18 +4,19 @@ import pkgutil
 import logging
 import yaml
 
+from sample.common.utils import load_logging_config
+
 logger = logging.getLogger(__name__)
 
-def load_logging_config() -> None:
-    data = pkgutil.get_data("sample", "data/logging.yaml")
-    logging_config = yaml.safe_load(data.decode("utf-8"))
-    logging.config.dictConfig(logging_config)
-    
+# def load_logging_config() -> None:
+#     data = pkgutil.get_data("sample", "data/logging.yaml")
+#     logging_config = yaml.safe_load(data.decode("utf-8"))
+#     logging.config.dictConfig(logging_config)
 
 
 def main():
     load_logging_config()
-    
+
     parser = argparse.ArgumentParser(description="CSVの結果を出力")
     parser.add_argument(
         "--foo",
@@ -28,6 +29,7 @@ def main():
     for line in args.foo:
         print(line)
     logger.info("出力しました。")
+
 
 if __name__ == "__main__":
     main()
---
+++ sample/common/utils.py
@@ -0,0 +1,9 @@
+import logging
+import yaml
+import pkgutil
+
+
+def load_logging_config() -> None:
+    data = pkgutil.get_data("sample", "data/logging.yaml")
+    logging_config = yaml.safe_load(data.decode("utf-8"))
+    logging.config.dictConfig(logging_config)
